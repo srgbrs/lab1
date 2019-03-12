d49a4152d6d96ac4d5a5e21deade2374509a7a65
srgbrs <43040660+srgbrs@users.noreply.github.com>
2019-03-13 01:52:47 +0200

    Initial Commit

---
+++ lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab1
+//
+//  Created by Sergey on 3/13/19.
+//  Copyright © 2019 sergey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
